df0de987875aa58a4cf07bbe632d1e5ee546932b
Oliver Tacke <o.tacke@posteo.de>
2025-07-14 11:00:44 +0200

    Fix path

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-import SustainumsLocalUserDataForH5P from './sustainum_local_user_data_for_h5p.js';
+import SustainumsLocalUserDataForH5P from './sustainums_local_user_data_for_h5p.js';
 new SustainumsLocalUserDataForH5P();
